f7c22f707d3bd90e730e2b8d9df2bf91cc3c4a83
dimasan92 <dimasan92@gmail.com>
2018-08-16 15:18:07 +0300

    main activity

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/presentation/view/activity/MainActivity.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/presentation/view/activity/MainActivity.java
@@ -1,15 +1,54 @@
 package ru.geekbrains.geekbrainsinstagram.presentation.view.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import ru.geekbrains.geekbrainsinstagram.R;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ru.geekbrains.geekbrainsinstagram.R;
+import ru.geekbrains.geekbrainsinstagram.presentation.utils.SettingsPrefsUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCurrentTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mCurrentTheme = SettingsPrefsUtils.getCurrentTheme(getApplicationContext());
+        setTheme(mCurrentTheme);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mCurrentTheme != SettingsPrefsUtils.getCurrentTheme(getApplicationContext())) {
+            recreate();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
