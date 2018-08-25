2d90c5caa76d3051bcea6a2b69b92e7bef0174f9
dimasan92 <dimasan92@gmail.com>
2018-08-25 14:52:32 +0300

    refactoring Navigator

---
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/navigator/INavigator.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.geekbrainsinstagram.ui.navigator;
+
+public interface INavigator {
+
+    void initializeView();
+
+    void navigateToPersonalPhotos();
+
+    void navigateToAppTheme();
+}
--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/navigator/Navigator.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/navigator/Navigator.java
@@ -5,63 +5,68 @@ import javax.inject.Inject;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import ru.geekbrains.geekbrainsinstagram.R;
-import ru.geekbrains.geekbrainsinstagram.di.activity.ActivityScope;
 import ru.geekbrains.geekbrainsinstagram.ui.screens.personalphotos.PersonalPhotosFragment;
 import ru.geekbrains.geekbrainsinstagram.ui.screens.theme.AppThemeFragment;
 
-@ActivityScope
-public final class Navigator {
+public final class Navigator implements INavigator {
 
-    private static final String COLOR_CHOOSER_TAG = "color_chooser_tag";
-    private static final String CAMERA_GALLERY_TAG = "camera_gallery_tag";
+    private static final String PERSONAL_PHOTOS_TAG = "personal_photos_tag";
+    private static final String APP_THEME_TAG = "color_chooser_tag";
 
-    private FragmentManager fragmentManager;
+    private final FragmentManager fragmentManager;
 
     @Inject
-    public Navigator() {
-    }
-
-    public void setFragmentManager(FragmentManager fragmentManager) {
+    public Navigator(FragmentManager fragmentManager) {
         this.fragmentManager = fragmentManager;
     }
 
+    @Override
     public void initializeView() {
-        Fragment fragment = fragmentManager.findFragmentById(R.id.main_fragment_container);
-
-        if (fragment == null) {
-            addFragmentWithoutBackStack(PersonalPhotosFragment.newInstance(), CAMERA_GALLERY_TAG);
+        if (fragmentManager.findFragmentById(R.id.main_fragment_container) == null) {
+            addFragmentWithoutBackStack(PersonalPhotosFragment.newInstance(), PERSONAL_PHOTOS_TAG);
         }
     }
 
-    public void navigateToCameraGallery() {
-        Fragment fragment = fragmentManager.findFragmentByTag(CAMERA_GALLERY_TAG);
+    @Override
+    public void navigateToPersonalPhotos() {
+        Fragment fragment = fragmentManager.findFragmentByTag(PERSONAL_PHOTOS_TAG);
         if (fragment == null) {
             fragment = PersonalPhotosFragment.newInstance();
         }
-        addFragmentWithoutBackStack(fragment, CAMERA_GALLERY_TAG);
+        if (fragment.isVisible()) {
+            return;
+        }
+        replaceFragmentWithoutBackStack(fragment, PERSONAL_PHOTOS_TAG);
     }
 
-    public void navigateToColorChooser() {
-        Fragment fragment = fragmentManager.findFragmentByTag(COLOR_CHOOSER_TAG);
+    @Override
+    public void navigateToAppTheme() {
+        Fragment fragment = fragmentManager.findFragmentByTag(APP_THEME_TAG);
         if (fragment == null) {
             fragment = AppThemeFragment.newInstance();
         }
         if (fragment.isVisible()) {
             return;
         }
-        addFragmentWithBackStack(fragment, COLOR_CHOOSER_TAG);
+        addFragmentWithBackStack(fragment, APP_THEME_TAG);
+    }
+
+    private void addFragmentWithoutBackStack(Fragment fragment, String tag) {
+        fragmentManager.beginTransaction()
+                .add(R.id.main_fragment_container, fragment, tag)
+                .commit();
+    }
+
+    private void replaceFragmentWithoutBackStack(Fragment fragment, String tag) {
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, fragment, tag)
+                .commit();
     }
 
     private void addFragmentWithBackStack(Fragment fragment, String tag) {
         fragmentManager.beginTransaction()
                 .replace(R.id.main_fragment_container, fragment, tag)
                 .addToBackStack(null)
-                .commit();
-    }
-
-    private void addFragmentWithoutBackStack(Fragment fragment, String tag) {
-        fragmentManager.beginTransaction()
-                .replace(R.id.main_fragment_container, fragment, tag)
                 .commit();
     }
 }
