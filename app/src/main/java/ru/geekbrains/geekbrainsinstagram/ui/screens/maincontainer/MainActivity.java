d01dc96acd4d93d1ab956290b3810171bfd190f8
dimasan92 <dimasan92@gmail.com>
2018-08-25 16:35:49 +0300

    refactoring main activity

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/maincontainer/MainActivity.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/maincontainer/MainActivity.java
@@ -2,7 +2,7 @@ package ru.geekbrains.geekbrainsinstagram.ui.screens.maincontainer;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -15,12 +15,12 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import ru.geekbrains.geekbrainsinstagram.MainApplication;
 import ru.geekbrains.geekbrainsinstagram.R;
-import ru.geekbrains.geekbrainsinstagram.ui.navigator.Navigator;
+import ru.geekbrains.geekbrainsinstagram.ui.navigator.INavigator;
 
 public final class MainActivity extends AppCompatActivity implements IMainPresenter.IView {
 
     @Inject
-    Navigator navigator;
+    INavigator navigator;
 
     @Inject
     IMainPresenter presenter;
@@ -29,11 +29,11 @@ public final class MainActivity extends AppCompatActivity implements IMainPresen
 
     private NavigationView.OnNavigationItemSelectedListener drawerListener = menuItem -> {
         switch (menuItem.getItemId()) {
-            case R.id.nav_main:
-                navigator.navigateToCameraGallery();
+            case R.id.nav_personal_photos:
+                navigator.navigateToPersonalPhotos();
                 break;
-            case R.id.nav_theme_chooser:
-                navigator.navigateToColorChooser();
+            case R.id.nav_app_theme:
+                navigator.navigateToAppTheme();
                 break;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -42,16 +42,17 @@ public final class MainActivity extends AppCompatActivity implements IMainPresen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setupComponent();
-        setTheme(presenter.setupTheme());
+        inject();
+        presenter.setupTheme(this::setTheme);
+
         super.onCreate(savedInstanceState);
         presenter.setView(this);
-        navigator.setFragmentManager(getSupportFragmentManager());
+
         setContentView(R.layout.activity_main);
-
-        setupActivityView();
-        navigator.initializeView();
-
+        setupView();
+        if (savedInstanceState == null) {
+            navigator.navigateToPersonalPhotos();
+        }
         presenter.viewIsReady();
     }
 
@@ -64,41 +65,48 @@ public final class MainActivity extends AppCompatActivity implements IMainPresen
         }
     }
 
-    private void setupComponent() {
+    private void inject() {
         MainApplication.getApp()
                 .getComponentsManager()
-                .initActivityComponent(this);
-        MainApplication.getApp()
-                .getComponentsManager()
-                .getActivityComponent().inject(this);
+                .getActivityComponent(this)
+                .inject(this);
     }
 
-    private void setupActivityView() {
-        Toolbar toolbar = setupToolbar();
+    private void setupView() {
+        Toolbar toolbar = setupAndGetToolbar();
         setupDrawer(toolbar);
-
-        NavigationView navigationView = findViewById(R.id.main_navigator);
-        navigationView.setNavigationItemSelectedListener(drawerListener);
     }
 
-    private Toolbar setupToolbar() {
+    private Toolbar setupAndGetToolbar() {
         Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         return toolbar;
     }
 
     private void setupDrawer(Toolbar toolbar) {
-        drawerLayout = findViewById(R.id.main_navigator_layout);
+        setupNavigationLayout(toolbar);
+        setupNavigationView();
+    }
 
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) drawerLayout.getLayoutParams();
-        params.width = metrics.widthPixels;
-        drawerLayout.setLayoutParams(params);
+    private void setupNavigationLayout(Toolbar toolbar) {
+        drawerLayout = findViewById(R.id.main_navigator_layout);
+        drawerLayout.setLayoutParams(adjustParamsForNavigationLayout(drawerLayout.getLayoutParams()));
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
                 toolbar, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    private void setupNavigationView() {
+        NavigationView navigationView = findViewById(R.id.main_navigator);
+        navigationView.setNavigationItemSelectedListener(drawerListener);
+    }
+
+    private ViewGroup.LayoutParams adjustParamsForNavigationLayout(ViewGroup.LayoutParams params) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        params.width = metrics.widthPixels;
+        return params;
     }
 }
