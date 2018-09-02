ad087859256339d99c2f2e9569906f7e1f96ed61
dimasan92 <dimasan92@gmail.com>
2018-09-02 18:08:38 +0300

    get foto uri from fileName

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/util/PictureUtils.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/util/PictureUtils.java
@@ -1,6 +1,5 @@
 package ru.geekbrains.geekbrainsinstagram.util;
 
-import android.net.Uri;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -13,14 +12,17 @@ import ru.geekbrains.geekbrainsinstagram.model.PresentPhotoModel;
 @Singleton
 public final class PictureUtils implements IPictureUtils {
 
+    private final IFilesUtils filesUtils;
+
     @Inject
-    PictureUtils() {
+    PictureUtils(IFilesUtils filesUtils) {
+        this.filesUtils = filesUtils;
     }
 
     @Override
-    public void loadImageIntoImageView(PresentPhotoModel photoModel, ImageView imageView) {
+    public void loadImageIntoImageView(PresentPhotoModel photo, ImageView imageView) {
         Picasso.get()
-                .load(Uri.parse(photoModel.getUri()))
+                .load(filesUtils.getUriForPhoto(photo))
                 .into(imageView);
     }
 }
