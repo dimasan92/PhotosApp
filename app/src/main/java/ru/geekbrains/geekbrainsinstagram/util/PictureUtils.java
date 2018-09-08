d5ebfd61eca2eeb6b23171c09f615f074d6ad2d8
dimasan92 <dimasan92@gmail.com>
2018-09-08 19:19:11 +0300

    add load placeholder and load error in picasso

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/util/PictureUtils.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/util/PictureUtils.java
@@ -7,6 +7,7 @@ import com.squareup.picasso.Picasso;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import ru.geekbrains.geekbrainsinstagram.R;
 import ru.geekbrains.geekbrainsinstagram.model.PresentPhotoModel;
 
 @Singleton
@@ -23,6 +24,8 @@ public final class PictureUtils implements IPictureUtils {
     public void loadImageIntoImageView(PresentPhotoModel photo, ImageView imageView) {
         Picasso.get()
                 .load(filesUtils.getUriForPhoto(photo))
+                .placeholder(R.drawable.ic_image_black_24dp)
+                .error(R.drawable.ic_error_black_24dp)
                 .into(imageView);
     }
 }
