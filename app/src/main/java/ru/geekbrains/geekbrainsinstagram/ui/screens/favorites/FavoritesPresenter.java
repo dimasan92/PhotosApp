1785f791fc962182ee9944ff0346121fa59847da
dimasan92 <dimasan92@gmail.com>
2018-09-09 10:21:09 +0300

    inject presenter is fixed

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/favorites/FavoritesPresenter.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/favorites/FavoritesPresenter.java
@@ -1,10 +1,18 @@
 package ru.geekbrains.geekbrainsinstagram.ui.screens.favorites;
 
+import javax.inject.Inject;
+
 import ru.geekbrains.geekbrainsinstagram.base.BasePresenter;
+import ru.geekbrains.geekbrainsinstagram.di.fragment.FragmentScope;
 import ru.geekbrains.geekbrainsinstagram.model.PresentPhotoModel;
 
+@FragmentScope
 public final class FavoritesPresenter extends BasePresenter<IFavoritesPresenter.IView>
         implements IFavoritesPresenter {
+
+    @Inject
+    FavoritesPresenter() {
+    }
 
     @Override
     public void deletePhotoFromFavorites(PresentPhotoModel photo) {
