4aaacf605898746be089f0f2c67f206bf9227156
dimasan92 <dimasan92@gmail.com>
2018-09-09 10:46:10 +0300

    rename method

--- data/src/main/java/ru/geekbrains/data/photos/PhotosDao.java
+++ data/src/main/java/ru/geekbrains/data/photos/PhotosDao.java
@@ -8,13 +8,14 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
+import io.reactivex.Single;
 import ru.geekbrains.data.photos.personalphotos.FavoritePhotoEntity;
 
 @Dao
 public interface PhotosDao {
 
     @Query("SELECT * FROM FavoritePhotoEntity")
-    List<FavoritePhotoEntity> getAllFavoritePhotos();
+    List<FavoritePhotoEntity> getAllFavorites();
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void addFavoritePhoto(FavoritePhotoEntity entity);
