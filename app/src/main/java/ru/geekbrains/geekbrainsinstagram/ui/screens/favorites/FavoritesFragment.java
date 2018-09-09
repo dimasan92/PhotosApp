189edc6487a11ef45c5ade0e9fac56fee6922ac4
dimasan92 <dimasan92@gmail.com>
2018-09-09 10:01:01 +0300

    realisation of recycler view is added

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/favorites/FavoritesFragment.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/favorites/FavoritesFragment.java
@@ -9,15 +9,27 @@ import javax.inject.Inject;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import ru.geekbrains.geekbrainsinstagram.MainApplication;
 import ru.geekbrains.geekbrainsinstagram.R;
 import ru.geekbrains.geekbrainsinstagram.base.BaseFragment;
+import ru.geekbrains.geekbrainsinstagram.model.PresentPhotoModel;
 import ru.geekbrains.geekbrainsinstagram.ui.mediator.IActivityToFragmentMediator;
+import ru.geekbrains.geekbrainsinstagram.util.ILayoutUtils;
+import ru.geekbrains.geekbrainsinstagram.util.IPictureUtils;
 
 public final class FavoritesFragment extends BaseFragment {
 
     @Inject
+    ILayoutUtils layoutUtils;
+
+    @Inject
+    IPictureUtils pictureUtils;
+
+    @Inject
     IActivityToFragmentMediator activityToFragmentMediator;
+
+    private FavoritesAdapter adapter;
 
     public static FavoritesFragment newInstance() {
         return new FavoritesFragment();
@@ -28,7 +40,9 @@ public final class FavoritesFragment extends BaseFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+
         inject();
+        initRecyclerView(view);
         activityToFragmentMediator.setupToolbar(view.findViewById(R.id.favorites_toolbar));
 
         return view;
@@ -36,5 +50,26 @@ public final class FavoritesFragment extends BaseFragment {
 
     private void inject() {
         MainApplication.getApp().getComponentsManager().getFragmentComponent().inject(this);
+    }
+
+    private void initRecyclerView(View layout) {
+        RecyclerView favoritesRecyclerView = layout.findViewById(R.id.photos_recycler_view);
+        favoritesRecyclerView.setLayoutManager(layoutUtils.getAdjustedGridLayoutManager());
+        adapter = new FavoritesAdapter(pictureUtils, adapterListener());
+        favoritesRecyclerView.setAdapter(adapter);
+    }
+
+    private FavoritesAdapter.IFavoriteListener adapterListener() {
+        return new FavoritesAdapter.IFavoriteListener() {
+            @Override
+            public void onDeleteFromFavoritesClick(PresentPhotoModel photo) {
+
+            }
+
+            @Override
+            public void onDeleteFromDeviceClick(PresentPhotoModel photo) {
+
+            }
+        };
     }
 }
