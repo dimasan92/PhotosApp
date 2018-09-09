9c64a84c2b57dd5e57e118b50098832d152d46e7
dimasan92 <dimasan92@gmail.com>
2018-09-09 10:10:19 +0300

    mechanism for inject presenter is added

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/di/fragment/module/FragmentModule.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/di/fragment/module/FragmentModule.java
@@ -3,12 +3,12 @@ package ru.geekbrains.geekbrainsinstagram.di.fragment.module;
 import dagger.Binds;
 import dagger.Module;
 import ru.geekbrains.geekbrainsinstagram.di.fragment.FragmentScope;
+import ru.geekbrains.geekbrainsinstagram.ui.mediator.FragmentToFragmentMediator;
 import ru.geekbrains.geekbrainsinstagram.ui.mediator.IFragmentToFragmentMediator;
-import ru.geekbrains.geekbrainsinstagram.ui.screens.personalphotos.IPersonalPhotosPresenter;
-import ru.geekbrains.geekbrainsinstagram.ui.screens.personalphotos.PersonalPhotosPresenter;
+import ru.geekbrains.geekbrainsinstagram.ui.screens.favorites.FavoritesPresenter;
+import ru.geekbrains.geekbrainsinstagram.ui.screens.favorites.IFavoritesPresenter;
 import ru.geekbrains.geekbrainsinstagram.ui.screens.theme.AppThemePresenter;
 import ru.geekbrains.geekbrainsinstagram.ui.screens.theme.IAppThemePresenter;
-import ru.geekbrains.geekbrainsinstagram.ui.mediator.FragmentToFragmentMediator;
 
 @Module
 public interface FragmentModule {
@@ -16,6 +16,10 @@ public interface FragmentModule {
     @FragmentScope
     @Binds
     IAppThemePresenter provideAppThemePresenter(AppThemePresenter presenter);
+
+    @FragmentScope
+    @Binds
+    IFavoritesPresenter provideFavoritesPresenter(FavoritesPresenter presenter);
 
     @FragmentScope
     @Binds
