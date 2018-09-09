b7c1bf266e70380107ba13719c110103b33a358f
dimasan92 <dimasan92@gmail.com>
2018-09-09 10:55:44 +0300

    get and add favorites on screen is added

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/favorites/FavoritesPresenter.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/favorites/FavoritesPresenter.java
@@ -2,16 +2,30 @@ package ru.geekbrains.geekbrainsinstagram.ui.screens.favorites;
 
 import javax.inject.Inject;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import ru.geekbrains.domain.interactor.photos.GetFavoritesUseCase;
 import ru.geekbrains.geekbrainsinstagram.base.BasePresenter;
 import ru.geekbrains.geekbrainsinstagram.di.fragment.FragmentScope;
 import ru.geekbrains.geekbrainsinstagram.model.PresentPhotoModel;
+import ru.geekbrains.geekbrainsinstagram.model.mapper.IPresentModelPhotosMapper;
 
 @FragmentScope
 public final class FavoritesPresenter extends BasePresenter<IFavoritesPresenter.IView>
         implements IFavoritesPresenter {
 
+    private final GetFavoritesUseCase getFavoritesUseCase;
+    private final IPresentModelPhotosMapper photosMapper;
+
     @Inject
-    FavoritesPresenter() {
+    FavoritesPresenter(GetFavoritesUseCase getFavoritesUseCase,
+                       IPresentModelPhotosMapper photosMapper) {
+        this.getFavoritesUseCase = getFavoritesUseCase;
+        this.photosMapper = photosMapper;
+    }
+
+    @Override
+    public void start() {
+        uploadPhotos();
     }
 
     @Override
@@ -22,5 +36,12 @@ public final class FavoritesPresenter extends BasePresenter<IFavoritesPresenter.
     @Override
     public void deletePhotoFromDevice(PresentPhotoModel photo) {
 
+    }
+
+    private void uploadPhotos() {
+        addDisposable(getFavoritesUseCase.execute()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(photos -> view.addPhotos(photosMapper.domainToView(photos)),
+                        getDefaultErrorHandler()));
     }
 }
