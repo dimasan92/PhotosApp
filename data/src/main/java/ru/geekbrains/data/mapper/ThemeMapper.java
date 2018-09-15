e1e371834d40156d3441159d882cdd0a134978fc
dimasan92 <dimasan92@gmail.com>
2018-09-15 11:51:55 +0300

    rename

--- data/src/main/java/ru/geekbrains/data/mapper/ThemeMapper.java
+++ data/src/main/java/ru/geekbrains/data/mapper/ThemeMapper.java
@@ -1,46 +1,10 @@
 package ru.geekbrains.data.mapper;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import ru.geekbrains.domain.model.AppThemeModel;
 
-@Singleton
-public class ThemeMapper implements IThemeMapper {
+public interface ThemeMapper {
 
-    private static final String RED_THEME = "red_application_theme";
-    private static final String BLUE_THEME = "blue_application_theme";
-    private static final String GREEN_THEME = "green_application_theme";
+    String domainToData(final AppThemeModel appTheme);
 
-    @Inject
-    ThemeMapper() {
-    }
-
-    @Override
-    public String domainToData(AppThemeModel appTheme) {
-        switch (appTheme) {
-            case RED_THEME:
-                return RED_THEME;
-            case BLUE_THEME:
-                return BLUE_THEME;
-            case GREEN_THEME:
-                return GREEN_THEME;
-            default:
-                throw new IllegalArgumentException("Wrong theme");
-        }
-    }
-
-    @Override
-    public AppThemeModel dataToDomain(String appTheme) {
-        switch (appTheme) {
-            case RED_THEME:
-                return AppThemeModel.RED_THEME;
-            case BLUE_THEME:
-                return AppThemeModel.BLUE_THEME;
-            case GREEN_THEME:
-                return AppThemeModel.GREEN_THEME;
-            default:
-                throw new IllegalArgumentException("Wrong theme");
-        }
-    }
+    AppThemeModel dataToDomain(final String appTheme);
 }
