b5d90cc6631a4b4e2df4686a4244328440f28df8
dimasan92 <dimasan92@gmail.com>
2018-09-15 11:49:42 +0300

    rename

--- data/src/main/java/ru/geekbrains/data/mapper/EntityPhotosMapper.java
+++ data/src/main/java/ru/geekbrains/data/mapper/EntityPhotosMapper.java
@@ -1,37 +1,15 @@
 package ru.geekbrains.data.mapper;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import ru.geekbrains.data.photos.personalphotos.FavoritePhotoEntity;
+import ru.geekbrains.data.photos.entities.FavoritePhotoEntity;
 import ru.geekbrains.domain.model.PhotoModel;
 
-@Singleton
-public final class EntityPhotosMapper implements IEntityPhotosMapper {
+public interface EntityPhotosMapper {
 
-    @Inject
-    EntityPhotosMapper() {
-    }
+    FavoritePhotoEntity domainToData(final PhotoModel photo);
 
-    @Override
-    public FavoritePhotoEntity domainToData(final PhotoModel photo) {
-        return new FavoritePhotoEntity(photo.getId());
-    }
+    List<PhotoModel> dataToDomain(final List<FavoritePhotoEntity> photos);
 
-    @Override
-    public List<PhotoModel> dataToDomain(final List<FavoritePhotoEntity> photos) {
-        List<PhotoModel> list = new ArrayList<>(photos.size());
-        for (FavoritePhotoEntity photo : photos) {
-            list.add(dataToDomain(photo));
-        }
-        return list;
-    }
-
-    @Override
-    public PhotoModel dataToDomain(final FavoritePhotoEntity photo) {
-        return new PhotoModel(photo.getId(), true);
-    }
+    PhotoModel dataToDomain(final FavoritePhotoEntity photo);
 }
