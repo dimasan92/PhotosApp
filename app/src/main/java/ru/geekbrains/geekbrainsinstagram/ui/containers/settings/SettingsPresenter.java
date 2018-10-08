8f26a59b9fe2512a3117ab29a2da411a49dde4c7
dimasan92 <dimasan92@gmail.com>
2018-10-08 17:25:07 +0300

    add base settings presenter

---
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/containers/settings/SettingsPresenter.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.geekbrainsinstagram.ui.containers.settings;
+
+import ru.geekbrains.geekbrainsinstagram.base.IBasePresenter;
+
+public interface SettingsPresenter extends IBasePresenter<SettingsPresenter.View> {
+
+    interface View extends IBasePresenter.IView {
+
+    }
+}
---
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/containers/settings/SettingsPresenterImpl.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.geekbrainsinstagram.ui.containers.settings;
+
+import ru.geekbrains.geekbrainsinstagram.base.BasePresenter;
+
+public final class SettingsPresenterImpl extends BasePresenter<SettingsPresenter.View> implements SettingsPresenter {
+
+
+}
