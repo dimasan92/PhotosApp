e744a0ce1055aa4fb7687567a5e7adc55205a2d0
dimasan92 <dimasan92@gmail.com>
2018-10-08 16:55:05 +0300

    insert new Activity for settings screens

---
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/containers/settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.geekbrainsinstagram.ui.containers.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.geekbrains.geekbrainsinstagram.R;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
