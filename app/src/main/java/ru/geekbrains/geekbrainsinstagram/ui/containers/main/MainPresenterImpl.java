e99f99ec23ea1e06efb238bd8562f1458f0f0baa
dimasan92 <dimasan92@gmail.com>
2018-10-13 18:12:27 +0300

    initialize constructor of base presenter

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/containers/main/MainPresenterImpl.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/containers/main/MainPresenterImpl.java
@@ -5,14 +5,16 @@ import javax.inject.Inject;
 import ru.geekbrains.domain.interactor.settings.GetCurrentThemeUseCase;
 import ru.geekbrains.geekbrainsinstagram.di.activity.main.MainActivityScope;
 import ru.geekbrains.geekbrainsinstagram.ui.containers.BaseContainerPresenterImpl;
+import ru.geekbrains.geekbrainsinstagram.ui.navigator.Screens;
+import ru.terrakok.cicerone.Router;
 
 @MainActivityScope
 public final class MainPresenterImpl extends BaseContainerPresenterImpl<MainPresenter.View>
         implements MainPresenter {
 
     @Inject
-    MainPresenterImpl(GetCurrentThemeUseCase getCurrentThemeUseCase) {
-        super(getCurrentThemeUseCase);
+    MainPresenterImpl(GetCurrentThemeUseCase getCurrentThemeUseCase, Router router, Screens screens) {
+        super(getCurrentThemeUseCase, router, screens);
     }
     @Override
     public void searchSelected(boolean isFromMainPageNavigationMenu) {
