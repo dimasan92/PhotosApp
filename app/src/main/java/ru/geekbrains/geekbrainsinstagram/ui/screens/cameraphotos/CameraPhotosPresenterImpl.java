6618fd692359da61a7f27f1b73581517656d8890
dimasan92 <dimasan92@gmail.com>
2018-10-13 18:36:55 +0300

    add injecting mechanism

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/cameraphotos/CameraPhotosPresenterImpl.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/cameraphotos/CameraPhotosPresenterImpl.java
@@ -1,9 +1,15 @@
 package ru.geekbrains.geekbrainsinstagram.ui.screens.cameraphotos;
 
-import ru.geekbrains.geekbrainsinstagram.base.BasePresenterImpl;
-import ru.geekbrains.geekbrainsinstagram.di.childfragment.ChildFragmentScope;
+import javax.inject.Inject;
 
+import ru.geekbrains.geekbrainsinstagram.base.BasePresenterImpl;
+import ru.geekbrains.geekbrainsinstagram.di.fragment.cameraphotos.CameraPhotosFragmentScope;
+
+@CameraPhotosFragmentScope
 public final class CameraPhotosPresenterImpl extends BasePresenterImpl<CameraPhotosPresenter.View>
         implements CameraPhotosPresenter {
 
+    @Inject
+    CameraPhotosPresenterImpl() {
+    }
 }
