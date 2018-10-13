d95962d3815f4b64a550cd0f5ed46f4a15f39581
dimasan92 <dimasan92@gmail.com>
2018-10-13 18:33:35 +0300

    add di for camera fragment

---
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/di/fragment/cameraphotos/CameraPhotosFragmentModule.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.geekbrainsinstagram.di.fragment.cameraphotos;
+
+import dagger.Binds;
+import dagger.Module;
+import ru.geekbrains.geekbrainsinstagram.ui.screens.cameraphotos.CameraPhotosPresenter;
+import ru.geekbrains.geekbrainsinstagram.ui.screens.cameraphotos.CameraPhotosPresenterImpl;
+
+@Module
+public interface CameraPhotosFragmentModule {
+
+    @CameraPhotosFragmentScope
+    @Binds
+    CameraPhotosPresenter provideAppThemePresenter(CameraPhotosPresenterImpl presenter);
+}
