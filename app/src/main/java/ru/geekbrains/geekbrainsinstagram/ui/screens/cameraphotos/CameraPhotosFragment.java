d1b728ac40f292f6d0c7732a15c5efd98868f566
dimasan92 <dimasan92@gmail.com>
2018-10-14 13:22:01 +0300

    initialize fragment

--- app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/cameraphotos/CameraPhotosFragment.java
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/ui/screens/cameraphotos/CameraPhotosFragment.java
@@ -1,7 +1,60 @@
 package ru.geekbrains.geekbrainsinstagram.ui.screens.cameraphotos;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import javax.inject.Inject;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import ru.geekbrains.geekbrainsinstagram.MainApplication;
+import ru.geekbrains.geekbrainsinstagram.R;
+import ru.geekbrains.geekbrainsinstagram.di.fragment.cameraphotos.CameraPhotosFragmentComponent;
+import ru.geekbrains.geekbrainsinstagram.util.LayoutUtils;
 
 public final class CameraPhotosFragment extends Fragment implements CameraPhotosPresenter.View {
 
+    @Inject
+    LayoutUtils layoutUtils;
+
+    @Inject
+    CameraPhotosPresenter presenter;
+
+    private CameraPhotosAdapter adapter;
+
+    public static CameraPhotosFragment newInstance() {
+        return new CameraPhotosFragment();
+    }
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photos_list, container, false);
+
+        inject();
+        initRecyclerView(view);
+
+        return view;
+    }
+
+    private void inject() {
+        CameraPhotosFragmentComponent component = (CameraPhotosFragmentComponent) MainApplication.getApp()
+                .getComponentsManager().getFragmentComponent(CameraPhotosFragment.class);
+        component.inject(this);
+    }
+
+    private void initRecyclerView(View layout) {
+        RecyclerView photoRecyclerView = layout.findViewById(R.id.photos_recycler_view);
+        photoRecyclerView.setLayoutManager(layoutUtils.getAdjustedGridLayoutManager());
+        adapter = new CameraPhotosAdapter();
+        photoRecyclerView.setAdapter(adapter);
+        if (photoRecyclerView.getItemAnimator() != null) {
+            photoRecyclerView.getItemAnimator().setChangeDuration(0);
+        }
+    }
 }
