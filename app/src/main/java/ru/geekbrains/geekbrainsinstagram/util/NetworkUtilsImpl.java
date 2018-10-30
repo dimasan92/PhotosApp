ca2bb43009350020cc24084dbdb7d966bb8526a6
dimasan92 <dimasan92@gmail.com>
2018-10-30 19:18:53 +0300

    Adding new utils for checking connection

---
+++ app/src/main/java/ru/geekbrains/geekbrainsinstagram/util/NetworkUtilsImpl.java
@@ -0,0 +1,86 @@
+package ru.geekbrains.geekbrainsinstagram.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import ru.geekbrains.data.util.NetworkUtils;
+
+@Singleton
+public final class NetworkUtilsImpl implements NetworkUtils {
+
+    enum NetworkState {
+        MOBILE,
+        WIFI,
+        ETHERNET,
+        OFFLINE
+    }
+
+    private final ConnectivityManager connectivityManager;
+    private NetworkState currentState = NetworkState.OFFLINE;
+
+    @Inject
+    NetworkUtilsImpl(final Context appContext) {
+        connectivityManager = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    @Override
+    public boolean isOnline() {
+        getState();
+        return currentState.equals(NetworkState.MOBILE) || currentState.equals(NetworkState.WIFI) ||
+                currentState.equals(NetworkState.ETHERNET);
+    }
+
+    @Override
+    public boolean isOffline() {
+        return NetworkState.OFFLINE.equals(getState());
+    }
+
+    @Override
+    public boolean isMobile() {
+        return NetworkState.MOBILE.equals(getState());
+    }
+
+    @Override
+    public boolean isWiFi() {
+        return NetworkState.WIFI.equals(getState());
+    }
+
+    @Override
+    public boolean isEthernet() {
+        return NetworkState.ETHERNET.equals(getState());
+    }
+
+    private NetworkState getState() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            final NetworkCapabilities networkCapabilities =
+                    connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                currentState = NetworkState.MOBILE;
+            } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                currentState = NetworkState.WIFI;
+            } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                currentState = NetworkState.ETHERNET;
+            } else {
+                currentState = NetworkState.OFFLINE;
+            }
+        } else {
+            final NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                currentState = NetworkState.MOBILE;
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                currentState = NetworkState.WIFI;
+            } else if (activeNetwork.getType() == ConnectivityManager.TYPE_ETHERNET) {
+                currentState = NetworkState.ETHERNET;
+            } else {
+                currentState = NetworkState.OFFLINE;
+            }
+        }
+        return currentState;
+    }
+}
