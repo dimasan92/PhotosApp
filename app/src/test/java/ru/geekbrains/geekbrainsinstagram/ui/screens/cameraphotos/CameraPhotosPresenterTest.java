1d8468b570c030b8f1c4ec15d722afbea6217a96
dimasan92 <dimasan92@gmail.com>
2018-10-31 01:59:26 +0300

    tests for camera screen presenters

--- app/src/test/java/ru/geekbrains/geekbrainsinstagram/ui/screens/cameraphotos/CameraPhotosPresenterTest.java
+++ app/src/test/java/ru/geekbrains/geekbrainsinstagram/ui/screens/cameraphotos/CameraPhotosPresenterTest.java
@@ -15,57 +15,39 @@ import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.schedulers.TestScheduler;
 import ru.geekbrains.domain.interactor.photos.CameraPhotoUpdaterUseCase;
+import ru.geekbrains.domain.interactor.photos.ChangePhotoFavoriteStatusUseCase;
 import ru.geekbrains.domain.interactor.photos.DeletePhotoUseCase;
 import ru.geekbrains.domain.interactor.photos.GetCameraPhotosUseCase;
 import ru.geekbrains.domain.interactor.photos.GetPlaceForNewCameraPhotoUseCase;
-import ru.geekbrains.domain.interactor.photos.SetFavoritePhotoStatusUseCase;
 import ru.geekbrains.domain.model.PhotoModel;
 import ru.geekbrains.geekbrainsinstagram.ui.base.photos.BaseListPresenter.ListView;
 import ru.geekbrains.geekbrainsinstagram.ui.screens.cameraphotos.CameraPhotosPresenter.CameraPhotosView;
+import ru.geekbrains.geekbrainsinstagram.ui.screens.cameraphotos.CameraPhotosPresenterImpl.CameraPhotosListPresenterImpl;
 import ru.geekbrains.geekbrainsinstagram.util.CameraUtils;
 
 public final class CameraPhotosPresenterTest {
 
     private CameraPhotosPresenter presenter;
+    private CameraPhotosListPresenterImpl listPresenter;
     private TestScheduler testScheduler;
 
-    @Mock CameraPhotosView view;
     @Mock GetCameraPhotosUseCase getCameraPhotosUseCase;
     @Mock GetPlaceForNewCameraPhotoUseCase getPlaceForNewCameraPhotoUseCase;
-    @Mock SetFavoritePhotoStatusUseCase setFavoritePhotoStatusUseCase;
+    @Mock ChangePhotoFavoriteStatusUseCase changePhotoFavoriteStatusUseCase;
     @Mock DeletePhotoUseCase deletePhotoUseCase;
     @Mock CameraPhotoUpdaterUseCase cameraPhotoUpdaterUseCase;
     @Mock CameraUtils cameraUtils;
 
+    @Mock CameraPhotosView mainView;
     @Mock ListView listView;
 
     @Before public void setup() {
         MockitoAnnotations.initMocks(this);
         testScheduler = new TestScheduler();
+        listPresenter = Mockito.spy(new CameraPhotosListPresenterImpl(changePhotoFavoriteStatusUseCase,
+                cameraPhotoUpdaterUseCase, testScheduler));
         presenter = Mockito.spy(new CameraPhotosPresenterImpl(getCameraPhotosUseCase, getPlaceForNewCameraPhotoUseCase,
-                setFavoritePhotoStatusUseCase, deletePhotoUseCase, cameraPhotoUpdaterUseCase, testScheduler, cameraUtils));
-    }
-
-    @Test public void updatesPhotosTest() {
-        final List<PhotoModel> photoModels = Arrays.asList(new PhotoModel("id1", "path1"),
-                new PhotoModel("id2", "path2"));
-        Mockito.when(getCameraPhotosUseCase.execute()).thenReturn(Single.just(photoModels));
-
-        presenter.attachListView(listView);
-        presenter.start();
-        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
-
-        Mockito.verify(listView).updatePhotos();
-    }
-
-    @Test public void takeAPhotoRequestFailedTest() {
-        Mockito.when(getPlaceForNewCameraPhotoUseCase.execute())
-                .thenReturn(Single.error(new RuntimeException("Wrong place")));
-
-        presenter.attachView(view);
-        presenter.takeAPhotoRequest();
-        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
-        Mockito.verify(view).showCouldNotLaunchCameraMessage();
+                deletePhotoUseCase, cameraPhotoUpdaterUseCase, listPresenter, testScheduler, cameraUtils));
     }
 
     @Test public void takeAPhotoTest() {
@@ -73,30 +55,28 @@ public final class CameraPhotosPresenterTest {
         final PhotoModel photoModel = new PhotoModel("id", "path");
         Mockito.when(getPlaceForNewCameraPhotoUseCase.execute()).thenReturn(Single.just(photoModel));
 
-        presenter.attachView(view);
+        presenter.attachView(mainView);
         presenter.attachListView(listView);
         presenter.setCameraResultOkCode(okCode);
 
         presenter.takeAPhotoRequest();
         testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
-        Mockito.verify(view).startCamera(photoModel.getFilePath());
+        Mockito.verify(mainView).startCamera(photoModel.getFilePath());
 
         presenter.cameraHasClosed(okCode);
-        Mockito.verify(listView).updatePhoto(0);
-        Mockito.verify(view).showPhotoSuccessAddedMessage();
+        Mockito.verify(listPresenter).addPhotoModel(photoModel);
+        Mockito.verify(mainView).showPhotoSuccessAddedMessage();
         Mockito.verify(cameraUtils).revokeCameraPermissions(photoModel.getFilePath());
     }
 
-    @Test public void deletePhotoFailedTest() {
-        final PhotoModel photoModel = new PhotoModel("id", "path");
-        Mockito.when(deletePhotoUseCase.execute(photoModel))
-                .thenReturn(Completable.error(new RuntimeException("Error deleting")));
+    @Test public void takeAPhotoRequestFailedTest() {
+        Mockito.when(getPlaceForNewCameraPhotoUseCase.execute())
+                .thenReturn(Single.error(new RuntimeException("Wrong place")));
 
-        presenter.attachView(view);
-        presenter.deletePhotoConfirm(photoModel);
+        presenter.attachView(mainView);
+        presenter.takeAPhotoRequest();
         testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
-
-        Mockito.verify(view).showErrorDeletingPhotoMessage();
+        Mockito.verify(mainView).showCouldNotLaunchCameraMessage();
     }
 
     @Test public void deletePhotoTest() {
@@ -106,14 +86,82 @@ public final class CameraPhotosPresenterTest {
         Mockito.when(deletePhotoUseCase.execute(photoModel)).thenReturn(Completable.complete());
         Mockito.when(getCameraPhotosUseCase.execute()).thenReturn(Single.just(photoModels));
 
-        presenter.attachView(view);
+        presenter.attachView(mainView);
         presenter.attachListView(listView);
         presenter.start();
         presenter.deletePhotoConfirm(photoModel);
         testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
 
-        Mockito.verify(listView).deletePhoto(0);
+        Mockito.verify(listPresenter).deletePhotoModel(photoModel);
         Mockito.verify(cameraPhotoUpdaterUseCase).execute();
-        Mockito.verify(view).showPhotoSuccessDeletedMessage();
+        Mockito.verify(mainView).showPhotoSuccessDeletedMessage();
+    }
+
+    @Test public void deletePhotoFailedTest() {
+        final PhotoModel photoModel = new PhotoModel("id", "path");
+        Mockito.when(deletePhotoUseCase.execute(photoModel))
+                .thenReturn(Completable.error(new RuntimeException("Error deleting")));
+
+        presenter.attachView(mainView);
+        presenter.deletePhotoConfirm(photoModel);
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+
+        Mockito.verify(mainView).showErrorDeletingPhotoMessage();
+    }
+
+    @Test public void onFavoritesClickTest() {
+        final List<PhotoModel> photoModels = Arrays.asList(new PhotoModel("id1", "path1"),
+                new PhotoModel("id2", "path2"));
+        final PhotoModel changedModel = new PhotoModel(photoModels.get(0), !photoModels.get(0).isFavorite());
+        Mockito.when(getCameraPhotosUseCase.execute()).thenReturn(Single.just(photoModels));
+        Mockito.when(changePhotoFavoriteStatusUseCase.execute(photoModels.get(0))).thenReturn(Single.just(changedModel));
+
+        presenter.attachView(mainView);
+        presenter.attachListView(listView);
+        presenter.start();
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+        Mockito.verify(listView).updatePhotos();
+
+        listPresenter.onFavoriteClick(0);
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+
+        Mockito.verify(listView).updatePhoto(0);
+        Mockito.verify(cameraPhotoUpdaterUseCase).execute();
+    }
+
+    @Test public void onFavoritesClickFailedTest() {
+        final List<PhotoModel> photoModels = Arrays.asList(new PhotoModel("id1", "path1"),
+                new PhotoModel("id2", "path2"));
+        Mockito.when(getCameraPhotosUseCase.execute()).thenReturn(Single.just(photoModels));
+        Mockito.when(changePhotoFavoriteStatusUseCase.execute(photoModels.get(0)))
+                .thenReturn(Single.error(new RuntimeException("Error change status")));
+
+        presenter.attachView(mainView);
+        presenter.attachListView(listView);
+        presenter.start();
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+        Mockito.verify(listView).updatePhotos();
+
+        listPresenter.onFavoriteClick(0);
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+
+        Mockito.verify(mainView).showErrorDeletingFromFavoritesMessage();
+    }
+
+    @Test public void onDeleteClickTest() {
+        final List<PhotoModel> photoModels = Arrays.asList(new PhotoModel("id1", "path1"),
+                new PhotoModel("id2", "path2"));
+        Mockito.when(getCameraPhotosUseCase.execute()).thenReturn(Single.just(photoModels));
+
+        presenter.attachView(mainView);
+        presenter.attachListView(listView);
+        presenter.start();
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+        Mockito.verify(listView).updatePhotos();
+
+        listPresenter.onDeleteClick(0);
+        testScheduler.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+
+        Mockito.verify(mainView).showPhotoDeleteDialog(photoModels.get(0));
     }
 }
