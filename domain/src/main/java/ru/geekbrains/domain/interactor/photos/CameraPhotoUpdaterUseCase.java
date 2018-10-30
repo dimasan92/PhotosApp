720129f7928440a95216311a596a085214717cef
dimasan92 <dimasan92@gmail.com>
2018-10-30 19:11:11 +0300

    Update photos while changing between screens

---
+++ domain/src/main/java/ru/geekbrains/domain/interactor/photos/CameraPhotoUpdaterUseCase.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.domain.interactor.photos;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+@Singleton
+public final class CameraPhotoUpdaterUseCase {
+
+    private final Subject<Boolean> updater;
+    private CompositeDisposable disposables;
+
+    @Inject CameraPhotoUpdaterUseCase() {
+        disposables = new CompositeDisposable();
+        updater = PublishSubject.create();
+    }
+
+    public void execute() {
+        updater.onNext(true);
+    }
+
+    public void subscribe(final Consumer<Boolean> next) {
+        disposables.add(updater.subscribe(next));
+    }
+
+    public void dispose() {
+        disposables.dispose();
+    }
+}
