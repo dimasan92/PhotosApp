ff28e37f870c90c917dc1975623517961438c45b
dimasan92 <dimasan92@gmail.com>
2018-12-01 16:41:19 +0300

    alignment annotations within entity

--- data/src/main/java/ru/geekbrains/pictureapp/data/database/entities/FavoriteEntity.java
+++ data/src/main/java/ru/geekbrains/pictureapp/data/database/entities/FavoriteEntity.java
@@ -8,19 +8,25 @@ import androidx.room.PrimaryKey;
 @Entity(tableName = "favorites")
 public final class FavoriteEntity {
 
-    @NonNull @PrimaryKey @ColumnInfo(name = "id") private final String id;
-    @NonNull @ColumnInfo(name = "file_path") private final String filePath;
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo(name = "id") private final String id;
+
+    @NonNull
+    @ColumnInfo(name = "file_path") private final String filePath;
 
     public FavoriteEntity(@NonNull final String id, @NonNull final String filePath) {
         this.id = id;
         this.filePath = filePath;
     }
 
-    @NonNull public String getId() {
+    @NonNull
+    public String getId() {
         return id;
     }
 
-    @NonNull public String getFilePath() {
+    @NonNull
+    public String getFilePath() {
         return filePath;
     }
 }
